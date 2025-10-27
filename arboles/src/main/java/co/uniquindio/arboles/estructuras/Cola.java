5bae4dbd4aed1d419682d66a390f50683a4b3106
wid <wilsonr.delgadov@uqvirtual.edu.co>
2025-10-26 22:24:18 -0500

    Modificacion colas ok

--- arboles/src/main/java/co/uniquindio/arboles/estructuras/Cola.java
+++ arboles/src/main/java/co/uniquindio/arboles/estructuras/Cola.java
@@ -1,20 +1,11 @@
 package co.uniquindio.arboles.estructuras;
 
-/**
- *
- * @param <T>
- */
 public class Cola<T> {
 
     public Nodo<T> nodoPrimero, nodoUltimo;
     public int tamanio;
 
 
-
-    /**
-     * Agrega un elemento en la Cola
-     * @param dato elemento a guardar en la Cola
-     */
     public void encolar(T dato) {
 
         Nodo<T> nodo = new Nodo<>(dato);
@@ -29,10 +20,7 @@ public class Cola<T> {
         tamanio++;
     }
 
-    /**
-     * Retorna y elimina el elemento que está al incio de la Cola
-     * @return Primer elemento de la Cola
-     */
+
     public T desencolar() {
 
         if(estaVacia()) {
@@ -50,50 +38,32 @@ public class Cola<T> {
         return dato;
     }
 
-    /**
-     * Verifica si la Cola está vacía
-     * @return true si está vacía
-     */
+
     public boolean estaVacia() {
         return nodoPrimero == null;
     }
 
 
-
-    /**
-     * Borra completamente la Cola
-     */
     public void borrarCola() {
         nodoPrimero = nodoUltimo = null;
         tamanio = 0;
     }
 
-    /**
-     * @return the primero
-     */
     public Nodo<T> getPrimero() {
         return nodoPrimero;
     }
 
-    /**
-     * @return the ultimo
-     */
+
     public Nodo<T> getUltimo() {
         return nodoUltimo;
     }
 
-    /**
-     * @return the tamano
-     */
+
     public int getTamano() {
         return tamanio;
     }
 
-    /**
-     * Verifica si la Cola es idéntica a la actual
-     * @param cola Cola a comparar
-     * @return True si son iguales
-     */
+
     public boolean sonIdenticas(Cola<T> cola) {
 
         Cola<T> clon1 = clone();
@@ -114,9 +84,6 @@ public class Cola<T> {
         return  true;
     }
 
-    /**
-     * Imprime una cola en consola
-     */
     public void imprimir() {
         Nodo<T> aux = nodoPrimero;
         while(aux!=null) {
